87e7cc973e54bd97e96251a9013355c5cfc48019
hamdiaya <ay.hamdi@esi-sba.dz>
2024-03-20 04:22:56 +0100

    set candidat informations done

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,4 +6,5 @@ const auth_candidat = require('../controllers/auth_candidat');
 router.post('/signup', auth_candidat.signUp);
 router.post('/sendConfirmationCode',auth_candidat.sendConfirmationCode);
 router.post('/verifyConfirmationCode',auth_candidat.verifyConfirmationCode);
+router.post('/setCandidatInfo',auth_candidat.setCandidatInfo);
 module.exports=router;
