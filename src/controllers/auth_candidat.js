6d874a609cf38258ee93679cc9751312ab77fdcd
hamdiaya <ay.hamdi@esi-sba.dz>
2024-03-22 02:26:44 +0100

    updated some functions

--- src/controllers/auth_candidat.js
+++ src/controllers/auth_candidat.js
@@ -17,13 +17,12 @@ const auth_controller={
         try {
           const hashedPassword=await bcrypt.hash(password,10);
           const user = await candidat.createCandidat(firstName,lastName,email,hashedPassword);
+          console.log(user);
           if(user!=null){
-           
             res.status(404).json('account already exist');
           }else{
             res.status(201).json('account created');
-          }
-          
+          } 
         } catch (error) {
           res.status(400).json({ error: error.message });
         }
@@ -34,10 +33,12 @@ const auth_controller={
         const { email } = req.body;
        
           try {
-            if(await candidat.sendConfirmationCodeByEmail(email)){
+            const data=await candidat.sendConfirmationCodeByEmail(email);
+            console.log(data);
+            if(data=="confirmation code sent successfully"){
               res.status(200).json({ message: 'Confirmation code sent' });
             }else{
-              res.status(400).json({ message:'email not found' });
+              res.status(400).json({ message:data.toString() });
             }
         
           } catch (error) {
@@ -52,11 +53,11 @@ const auth_controller={
  verifyConfirmationCode: async (req, res) => {
   const { email, confirmationCode } = req.body;
   try {
-    const verified = await candidat.verifyConfirmationCode(email, confirmationCode);
-    if (verified) {
-      res.status(200).json('verified');
+    const data = await candidat.verifyConfirmationCode(email, confirmationCode);
+    if (data=="user verified") {
+      res.status(200).json('user verified successfully');
     } else {
-      res.status(400).json('not verified');
+      res.status(400).json({message:data.toString()});
     }
   } catch (error) {
     res.status(500).json('error');
@@ -64,17 +65,17 @@ const auth_controller={
 },
 
 setCandidatInfo: async (req, res) => {
-  const { email, firstName_fr, lastName_fr, sexe, date_of_birth, numéro_national, father_name_arabe, mother_first_name_arabe, mother_last_name_arabe, wilaya_résidence, commune_résidence } = req.body;
+  const { email, firstName_fr, lastName_fr, sexe, date_of_birth, numéro_national, father_name_arabe, mother_first_name_arabe, mother_last_name_arabe, wilaya_résidence, commune_résidence,ancienté } = req.body;
   try {
     const formattedDate = moment(date_of_birth, 'DD/MM/YYYY').format('YYYY-MM-DD');
-    const result = await candidat.setCandidatInfo(email, firstName_fr, lastName_fr, sexe, formattedDate, numéro_national, father_name_arabe, mother_first_name_arabe, mother_last_name_arabe, wilaya_résidence, commune_résidence);
-    if (result) {
+    const result = await candidat.setCandidatInfo(email, firstName_fr, lastName_fr, sexe, formattedDate, numéro_national, father_name_arabe, mother_first_name_arabe, mother_last_name_arabe, wilaya_résidence, commune_résidence,ancienté);
+    if (result=="candidate informations added successfully") {
       res.status(200).json({ message: 'Candidat info updated successfully' });
     } else {
-      res.status(404).json({ error: 'User not found or error updating candidat info' });
+      res.status(404).json({ error: result.toString() });
     }
   } catch (error) {
-    console.error('Error setting candidat info:', error.message);
+   
     res.status(500).json({ error: 'Internal server error' });
   }
 },
@@ -91,7 +92,7 @@ login: async (req, res) => {
           // If user doesn't exist, return a 404 error
           return res.status(404).json({ error: 'User not found' });
       }
-      console.log(existingUser.password);
+     
       const passwordMatch = await bcrypt.compare(password, existingUser.password);
 
       if (!passwordMatch) {
@@ -128,6 +129,8 @@ login: async (req, res) => {
       res.status(500).json({ error: 'Internal server error' });
   }
 },
+
+
   addMahram:async(req,res)=>{
    const {email,numéro_national_mahram}=req.body;
    try {
@@ -135,17 +138,17 @@ login: async (req, res) => {
 
       if (!existingUser||!existingUser.email) {
           // If user doesn't exist, return a 404 error
-          console.log('hh');
           return res.status(404).json({ error: 'User not found' });
       }
       if(existingUser.sexe=="ذكر"){
         return res.status(404).json({error:'تم تحديد الجنس: ذكر,هذا الحقل لا يخصك'});
       }else{
         if(existingUser.sexe=="انثى"){
-          if(await candidat.linkToMahram(email,numéro_national_mahram)){
+          const data=await candidat.linkToMahram(email,numéro_national_mahram);
+          if(data==" Successfully linked to Mahram"){
            return res.status(200).json('mahram added');
           }else{
-           return res.status(404).json('error while adding mahram ');
+           return res.status(404).json({message:data.toString()});
           }
         }
       }
