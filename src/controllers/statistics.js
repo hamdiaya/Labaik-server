9e4ce3ac1eb5f7da6dca9b0f5f0b607b83b8e600
hamdiaya <ay.hamdi@esi-sba.dz>
2024-05-20 01:23:07 +0200

    statitics done

--- src/controllers/statistics.js
+++ src/controllers/statistics.js
@@ -3,37 +3,68 @@ const candidat = require("../models/candidat");
 const selected_candidat = require("../models/selected_candidat");
 const statistics_controller = {
   getStatistics: async (req, res) => {
-    let wilayas=[];
+    let wilayas = [];
     const totalCandidats = await candidat.getTotalNumberOfCandidat();
+    if (totalCandidats == "error") {
+      res.status(404).json("error fetching statistics");
+    }
     const totalAcceptedCandidats =
       await selected_candidat.getTotalNumberOfAcceptedCandidat();
+    if (totalAcceptedCandidats == "error") {
+      res.status(404).json("error fetching statistics");
+    }
     const totalAcceptedCandidatsByDoctor =
       await selected_candidat.getTotalNumberOfAcceptedCandidatAfterMedicalVisite();
+    if (totalAcceptedCandidatsByDoctor == "error") {
+      res.status(404).json("error fetching statistics");
+    }
     const totalAcceptedCandidatsPayé =
       await selected_candidat.getTotalNumberOfAcceptedCandidatPayé();
-      for (let i = 1; i <= 58; i++) {
-       var totalCandidatsOfWilaya=await candidat.getNumberOfCandidatOfWilaya(i);
-       var totalAcceptedCandidatsOfWilaya=await selected_candidat.getTotalNumberOfAcceptedCandidatOfWilaya(i);
-       var totalAcceptedCandidatsByDoctorOfWilaya=await selected_candidat.getTotalNumberOfAcceptedCandidatAfterMedicalVisiteOfWilaya(i);
-       var totalAcceptedCandidatsPayéOfWilaya=await selected_candidat.getTotalNumberOfAcceptedCandidatPayéOfWilaya(i);
-
-       wilayas.push({
-        wilaya:i,
-        totalCandidatsOfWilaya:totalCandidatsOfWilaya,
-        totalAcceptedCandidatsOfWilaya: totalAcceptedCandidatsOfWilaya,
-        totalAcceptedCandidatsByDoctorOfWilaya: totalAcceptedCandidatsByDoctorOfWilaya,
-        totalAcceptedCandidatsPayéOfWilaya: totalAcceptedCandidatsPayéOfWilaya,
-       });
+    if (totalAcceptedCandidatsPayé == "error") {
+      res.status(404).json("error fetching statistics");
+    }
+    for (let i = 1; i <= 58; i++) {
+      var totalCandidatsOfWilaya = await candidat.getNumberOfCandidatOfWilaya(
+        i
+      );
+      if (totalCandidatsOfWilaya == "error") {
+        res.status(404).json("error fetching statistics");
       }
-    res
-      .status(200)
-      .json({
-        totalCandidats,
-        totalAcceptedCandidats,
-        totalAcceptedCandidatsByDoctor,
-        totalAcceptedCandidatsPayé,
-       wilayas
+
+      var totalAcceptedCandidatsOfWilaya =
+        await selected_candidat.getTotalNumberOfAcceptedCandidatOfWilaya(i);
+      if (totalAcceptedCandidatsOfWilaya == "error") {
+        res.status(404).json("error fetching statistics");
+      }
+
+      var totalAcceptedCandidatsByDoctorOfWilaya =
+        await selected_candidat.getTotalNumberOfAcceptedCandidatAfterMedicalVisiteOfWilaya(
+          i
+        );
+      if (totalAcceptedCandidatsByDoctorOfWilaya == "error") {
+        res.status(404).json("error fetching statistics");
+      }
+      var totalAcceptedCandidatsPayéOfWilaya =
+        await selected_candidat.getTotalNumberOfAcceptedCandidatPayéOfWilaya(i);
+      if (totalAcceptedCandidatsPayéOfWilaya == "error") {
+        res.status(404).json("error fetching statistics");
+      }
+      wilayas.push({
+        wilaya: i,
+        totalCandidatsOfWilaya: totalCandidatsOfWilaya,
+        totalAcceptedCandidatsOfWilaya: totalAcceptedCandidatsOfWilaya,
+        totalAcceptedCandidatsByDoctorOfWilaya:
+          totalAcceptedCandidatsByDoctorOfWilaya,
+        totalAcceptedCandidatsPayéOfWilaya: totalAcceptedCandidatsPayéOfWilaya,
       });
+    }
+    res.status(200).json({
+      totalCandidats,
+      totalAcceptedCandidats,
+      totalAcceptedCandidatsByDoctor,
+      totalAcceptedCandidatsPayé,
+      wilayas,
+    });
   },
 };
 module.exports = statistics_controller;
--- src/routes/statistics.js
+++ src/routes/statistics.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const statistics_controller=require('../controllers/statistics');
-router.get("/total", statistics_controller.getStatistics );
+router.get("", statistics_controller.getStatistics );
 module.exports=router;
